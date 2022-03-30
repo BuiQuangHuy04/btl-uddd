1b01d8def43db65d5c537734003ab518793e7b5e
Admin <hoangtrungson07012001@gmail.com>
2022-03-30 16:41:12 +0700

    MAIIIIIIIII

--- app/src/main/java/com/example/foodapp/Adapters/IngredientsAdapter.java
+++ app/src/main/java/com/example/foodapp/Adapters/IngredientsAdapter.java
@@ -16,7 +16,7 @@ import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
-public class IngredientsAdapter extends  RecyclerView.Adapter<IngredientsViewHoder> {
+public class IngredientsAdapter extends RecyclerView.Adapter<IngredientsViewHoder> {
 
     Context context;
     List<ExtendedIngredient> list;
@@ -29,18 +29,18 @@ public class IngredientsAdapter extends  RecyclerView.Adapter<IngredientsViewHod
     @NonNull
     @Override
     public IngredientsViewHoder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return new IngredientsViewHoder(LayoutInflater.from(context).inflate(R.layout.list_meal_ingredients,parent, false));
+        return new IngredientsViewHoder(LayoutInflater.from(context).inflate(R.layout.list_meal_ingredients, parent, false));
 
     }
 
     @Override
     public void onBindViewHolder(@NonNull IngredientsViewHoder holder, int position) {
-  holder.textView_ingredients_name.setText(list.get(position).name);
- holder.textView_ingredients_name.setSelected(true);
-  holder.textView_ingredients_quantity.setText(list.get(position).original);
- holder.textView_ingredients_quantity.setSelected(true);
-
-        Picasso.get().load("http://spoonacular.com/cdn/ingredients_100x100/"+list.get(position).image).into(holder.imageView_ingredients);
+        holder.textView_ingredients_name.setText(list.get(position).name);
+        holder.textView_ingredients_name.setSelected(true);
+        holder.textView_ingredients_quantity.setText(list.get(position).original);
+        holder.textView_ingredients_quantity.setSelected(true);
+        Picasso.get().load("https://spoonacular.com/cdn/ingredients_100x100/"+list.get(position).image)
+                .into(holder.imageView_ingredients);
 
     }
 
@@ -49,14 +49,16 @@ public class IngredientsAdapter extends  RecyclerView.Adapter<IngredientsViewHod
         return list.size();
     }
 }
-class IngredientsViewHoder extends RecyclerView.ViewHolder{
-    TextView textView_ingredients_quantity,textView_ingredients_name;
+
+class IngredientsViewHoder extends RecyclerView.ViewHolder {
+    TextView textView_ingredients_quantity, textView_ingredients_name;
     ImageView imageView_ingredients;
+
     public IngredientsViewHoder(@NonNull View itemView) {
         super(itemView);
-        textView_ingredients_quantity= itemView.findViewById(R.id.textView_ingredients_quantity);
-        textView_ingredients_name= itemView.findViewById(R.id.textView_ingredients_name);
-        imageView_ingredients= itemView.findViewById(R.id.imageView_ingredients);
+        textView_ingredients_quantity = itemView.findViewById(R.id.textView_ingredients_quantity);
+        textView_ingredients_name = itemView.findViewById(R.id.textView_ingredients_name);
+        imageView_ingredients = itemView.findViewById(R.id.imageView_ingredients);
 
     }
 
