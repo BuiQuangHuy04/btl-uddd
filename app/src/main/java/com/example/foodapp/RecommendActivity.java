8dd9684d6b7880a569c859aea20e911dd583a658
Bui Huy <74649569+BuiQuangHuy04@users.noreply.github.com>
2022-04-24 16:41:35 +0700

    final

--- app/src/main/java/com/example/foodapp/RecommendActivity.java
+++ app/src/main/java/com/example/foodapp/RecommendActivity.java
@@ -4,15 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.TextView;
-
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-
 public class RecommendActivity extends AppCompatActivity {
-    Button btn_GoTo;
-    TextView tv_Welcome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,10 +13,5 @@ public class RecommendActivity extends AppCompatActivity {
         findViewById(R.id.btn_GoTo).setOnClickListener(view->{
             startActivity(new Intent(RecommendActivity.this, SignInActivity.class));
         });
-        // Write a message to the database
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
-
-        myRef.setValue("Hello, World!");
     }
 }
